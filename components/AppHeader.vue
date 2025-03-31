617f9a14cae54ec4e9ab4e6ed648853c8692d144
alkaline1024 <earthza991@hotmail.com>
2025-03-31 18:51:02 +0700

    feat: 🎸 change AppHeader to anchor link

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,47 +1,72 @@
 <script setup lang="ts">
-const route = useRoute();
-const router = useRouter();
-const localePath = useLocalePath();
 const { t } = useI18n();
+
 const tabs = computed(() => [
-  { name: t("home"), path: "/", icon: "ic:outline-home", activeIcon: "ic:round-home" },
-  { name: t("projects"), path: "/projects", icon: "ic:round-star-border", activeIcon: "ic:round-star" },
-  { name: t("resume"), path: "/resume", icon: "ic:outline-contact-page", activeIcon: "ic:round-contact-page" },
-  { name: t("blog"), path: "/blog", icon: "ic:outline-message", activeIcon: "ic:message" },
-  { name: t("contact"), path: "/contact", icon: "ic:outline-local-phone", activeIcon: "ic:local-phone" },
+  { name: t("home"), target: "home", icon: "ic:outline-home", activeIcon: "ic:round-home" },
+  { name: t("projects"), target: "projects", icon: "ic:round-star-border", activeIcon: "ic:round-star" },
+  { name: t("resume"), target: "resume", icon: "ic:outline-contact-page", activeIcon: "ic:round-contact-page" },
+  { name: t("blog"), target: "blog", icon: "ic:outline-message", activeIcon: "ic:message" },
+  { name: t("contact"), target: "contact", icon: "ic:outline-local-phone", activeIcon: "ic:local-phone" },
 ]);
-const isActive = (path: string) => route.path === localePath(path);
+
+const activeTab = ref("home");
+
+const isActive = (target: string) => activeTab.value === target;
+
+let observer: IntersectionObserver | null = null;
+
+onMounted(() => {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.6,
+  };
+
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        activeTab.value = entry.target.id;
+      }
+    });
+  }, options);
+
+  tabs.value.forEach((tab) => {
+    const element = document.getElementById(tab.target);
+    if (element && observer) {
+      observer.observe(element);
+    }
+  });
+});
+
+onBeforeUnmount(() => {
+  if (observer) {
+    observer.disconnect();
+  }
+});
 </script>
 
 <template>
-  <div class="h-[64px] w-full flex justify-between border-b border-black/15 dark:border-white/15">
+  <div
+    class="flex h-[64px] w-full justify-end border-b border-black/15 bg-white/75 backdrop-blur-sm dark:border-white/15 dark:bg-stone-900/75"
+  >
     <div
       v-for="tab in tabs"
-      class="relative w-full h-full place-content-center"
+      :key="tab.target"
+      class="relative h-full w-fit place-content-center"
     >
-      <div
-        class="z-10 absolute bg-primary left-0 right-0 top-0 bottom-0 cursor-pointer text-lg text-primary border-b-2 flex items-center justify-center gap-2"
-        :class="{
-          'border-primary': isActive(tab.path),
-          'border-transparent opacity-80  hover:opacity-100': !isActive(tab.path),
-        }"
-        @click="router.push(localePath(tab.path))"
+      <a
+        class="z-10 flex cursor-pointer items-center justify-center gap-2 px-4 text-lg"
+        :href="`#${tab.target}`"
+        :class="isActive(tab.target) ? 'bg-secondary rounded-full p-1 text-white' : 'hover:bg-primary/25 text-primary'"
       >
         <UIcon
           class="text-2xl"
-          :name="isActive(tab.path) ? tab.activeIcon : tab.icon"
+          :name="isActive(tab.target) ? tab.activeIcon : tab.icon"
         />
         {{ tab.name }}
-      </div>
-      <div
-        :class="{
-          'shadow-primary': isActive(tab.path),
-          'shadow-transparent': !isActive(tab.path),
-        }"
-        class="z-0 absolute top-0 bottom-0 left-0 right-0 shadow-lg duration-400"
-      />
+      </a>
     </div>
-    <div class="flex items-center justify-center gap-4 p-4 bg-primary z-10">
+    <div class="bg-primary z-10 flex items-center justify-center gap-4 p-4">
       <ChangeLanguageButton />
       <ColorModeButton />
     </div>
