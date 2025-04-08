94910a7a535d617347a7ba5bfb4fe825d644ee98
alkaline1024 <earthza991@hotmail.com>
2025-04-08 16:03:54 +0700

    feat: 🎸 add scroll animation on click AppHeader tab

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { useWindowScroll } from "@vueuse/core";
-
 const { t } = useI18n();
 
 // prettier-ignore
@@ -8,11 +6,40 @@ const tabs = computed(() => [
   { id: "header-tab-home", name: t("home"), target: "home", icon: "ic:outline-home", activeIcon: "ic:round-home" },
   { id: "header-tab-skills", name: t("skills"), target: "skills", icon: "ic:round-star-border", activeIcon: "ic:round-star" },
   { id: "header-tab-education", name: t("education"), target: "education", icon: "ic:outline-school", activeIcon: "ic:round-school" },
-  { id: "header-tab-experiences",name: t("experiences"), target: "experiences", icon: "ic:round-work-outline", activeIcon: "ic:round-work" },
-  { id: "header-tab-projects",name: t("projects"), target: "projects", icon: "ic:round-work-outline", activeIcon: "ic:round-work" },
+  { id: "header-tab-experiences",name: t("work-experiences"), target: "experiences", icon: "ic:round-work-outline", activeIcon: "ic:round-work" },
+  { id: "header-tab-projects",name: t("projects"), target: "projects", icon: "ic:outline-workspace-premium", activeIcon: "ic:round-workspace-premium" },
   { id: "header-tab-blogs",name: t("blogs"), target: "blogs", icon: "ic:outline-message", activeIcon: "ic:message" },
   { id: "header-tab-contact",name: t("contact"), target: "contact", icon: "ic:outline-local-phone", activeIcon: "ic:local-phone" },
 ]);
+
+const scrolling = ref(false);
+const activeTab = ref("home");
+const isActive = (target: string) => activeTab.value === target;
+
+const buffer = window.innerHeight / 4;
+const highlightTabStyle = ref();
+
+function updateHighlightTab() {
+  if (scrolling.value) {
+    return true;
+  }
+  const targets = document.querySelectorAll<HTMLElement>(".section");
+  const currentPosition = window.scrollY;
+  for (const target of targets) {
+    if (currentPosition < target.offsetTop + target.offsetHeight - buffer) {
+      activeTab.value = target.id;
+      const tabElement = document.getElementById(`header-tab-${target.id}`);
+      if (!tabElement) {
+        continue;
+      }
+      highlightTabStyle.value = {
+        left: `${tabElement.offsetLeft}px`,
+        width: `${tabElement.offsetWidth}px`,
+      };
+      return true;
+    }
+  }
+}
 
 function scrollToTarget(id: string) {
   const target = document.getElementById(id);
@@ -21,57 +48,37 @@ function scrollToTarget(id: string) {
       top: target.offsetTop - 72,
       behavior: "smooth",
     });
-  }
-}
-
-const activeTab = ref("home");
-const isActive = (target: string) => activeTab.value === target;
-
-const targets = ref<HTMLElement[]>([]);
-for (const tab of tabs.value) {
-  const target = document.getElementById(tab.target);
-  if (target) {
-    targets.value.push(target);
-  }
-}
-
-const buffer = window.innerHeight / 4;
-const { x, y } = useWindowScroll();
-const highlightTabStyle = ref();
-
-watch(y, (currentPosition) => {
-  console.log(y.value);
-  for (const target of targets.value) {
-    if (currentPosition < target.offsetTop + target.offsetHeight - buffer) {
-      activeTab.value = target.id;
-      const tabElement = document.getElementById(`header-tab-${target.id}`);
-      console.log(tabElement);
-      if (!tabElement) {
-        continue;
-      }
+    const tabElement = document.getElementById(`header-tab-${id}`);
+    if (tabElement) {
+      scrolling.value = true;
       highlightTabStyle.value = {
         left: `${tabElement.offsetLeft}px`,
         width: `${tabElement.offsetWidth}px`,
       };
-      break;
     }
   }
+}
+
+onMounted(() => {
+  window.addEventListener("resize", updateHighlightTab);
+  window.addEventListener("scroll", updateHighlightTab);
+  window.addEventListener("scrollend", () => (scrolling.value = false));
 });
 </script>
 
 <template>
   <div
-    class="difference fixed flex h-[64px] w-full justify-end border-b border-black/15 bg-white/50 backdrop-blur-xl dark:bg-stone-900/50"
+    class="difference fixed flex h-[64px] w-full justify-end border-b border-black/15 bg-gray-100/75 shadow-lg shadow-black/5 backdrop-blur-xl dark:bg-stone-900/50"
   >
     <div
-      class="light:bg-green-500 absolute top-3 z-0 rounded-full p-5 opacity-90 transition-all dark:bg-green-600"
+      class="light:bg-green-400 absolute top-3 z-0 rounded-full p-5 opacity-90 transition-all dark:bg-green-600"
       :style="highlightTabStyle"
     />
     <div
       v-for="tab in tabs"
       :id="tab.id"
       :key="tab.target"
-      class="relative h-full w-fit place-content-center"
+      class="header-tab relative h-full w-fit place-content-center"
       @click="scrollToTarget(tab.target)"
     >
       <div
@@ -80,12 +87,12 @@ watch(y, (currentPosition) => {
       >
         <UIcon
           class="text-2xl"
-          :name="isActive(tab.target) ? tab.activeIcon : tab.icon"
+          :name="tab.icon"
         />
         {{ tab.name }}
       </div>
     </div>
-    <div class="bg-primary z-10 flex items-center justify-center gap-4 p-4">
+    <div class="z-10 flex items-center justify-center gap-4 p-4">
       <ChangeLanguageButton />
       <ColorModeButton />
     </div>
