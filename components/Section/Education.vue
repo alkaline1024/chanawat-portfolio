44a4f319ea90dabcd4fb23d140d509af459a2b91
alkaline1024 <earthza991@hotmail.com>
2025-04-10 13:35:20 +0700

    feat: 🎸 responsive Education in mobile

--- components/Section/Education.vue
+++ components/Section/Education.vue
@@ -7,24 +7,30 @@ const { t, locale } = useI18n();
     <h1 class="font-medium">
       {{ t("education") }}
     </h1>
-    <div class="mt-8 flex items-center gap-8">
-      <img
-        src="/img/PSU-Emblem.png"
-        class="w-28 drop-shadow-lg"
-      />
-      <div class="space-y-2">
+    <div class="bg-primary mt-8 flex items-center gap-4 overflow-hidden rounded-xl max-md:flex-col max-md:text-center">
+      <div class="max-md:light:bg-gradient-to-br h-40 md:p-4 max-md:w-full md:h-56">
+        <img
+          src="/img/PSU-Emblem.png"
+          class="size-full object-contain drop-shadow-lg"
+        />
+      </div>
+      <div class="space-y-2 p-4">
         <h3 class="font-medium">
           {{ locale == "th" ? "มหาวิทยาลัยสงขลานครินทร์" : "Prince of Songkhla University" }}
         </h3>
-        <p class="font-medium">
-          {{
-            locale == "th"
-              ? "หลักสูตรวิศวกรรมศาสตรบัณฑิต สาขาวิชาวิศวกรรมคอมพิวเตอร์"
-              : "Bachelor of Engineer in Computer Engineer"
-          }}
-        </p>
-        <p class="opacity-70">{{ locale == "th" ? "มิถุนายน 2564 - มีนาคม 2568" : "June 2021 - March 2025" }}</p>
-        <p>{{ locale == "th" ? "ผลการเรียนเฉลี่ย: 3.64" : "Cumulative GPA: 3.64" }}</p>
+        <div class="font-medium text-pretty">
+          <div v-if="locale == 'th'">
+            <p>
+              หลักสูตรวิศวกรรมศาสตรบัณฑิต <br class="md:hidden" />
+              สาขาวิชาวิศวกรรมคอมพิวเตอร์
+            </p>
+          </div>
+          <div v-else>
+            <p>Bachelor of Engineer in Computer Engineer</p>
+          </div>
+        </div>
+        <div class="opacity-70">{{ locale == "th" ? "มิถุนายน 2564 - มีนาคม 2568" : "June 2021 - March 2025" }}</div>
+        <div>{{ locale == "th" ? "ผลการเรียนเฉลี่ย: 3.64" : "Cumulative GPA: 3.64" }}</div>
       </div>
     </div>
   </div>
